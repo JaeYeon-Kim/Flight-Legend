ce23583124360f11cfa4a85ac72eb97646dbc6bb
JaeYoun Kim <rlawodusx@naver.com>
2024-07-31 00:42:35 +0900

    Fix: 이미 풀에 반환되었을경우 에러 처리 추가

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -21,12 +21,15 @@ public class Projectile : MonoBehaviour
     }
 
     // 오브젝트 풀로 가져온 Projectitle에 새로운 데미지를 설정하는 메서드
-    public void SetDamage(int weaponDamage) {
+    public void SetDamage(int weaponDamage)
+    {
         this.damage = weaponDamage;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if (!gameObject.activeSelf) return; // 이미 비활성화된 경우 처리 중단
 
         Debug.Log("변화한 공격력" + this.damage);
 
@@ -35,21 +38,21 @@ public class Projectile : MonoBehaviour
         {
             // 부딪힌 오브젝트 데미지 주는 처리 
             collision.GetComponent<EnemyHP>().TakeDamage(this.damage);
-
-            // 오브젝트 풀에 반환
-            Pool.Release(this.gameObject);
         }
         // 보스 몬스터 충돌처리
         else if (collision.CompareTag("Boss"))
         {
             collision.GetComponent<BossData>().TakeDamage(this.damage);
-
-            // 오브젝트 풀에 반환
-            Pool.Release(this.gameObject);
         }
         else
         {
             return;
+        }
+
+        // 오브젝트 풀에 반환
+        if (Pool != null)
+        {
+            Pool.Release(this.gameObject);
         }
 
     }
@@ -58,12 +61,18 @@ public class Projectile : MonoBehaviour
     // LateUpdate에서 특정 오브젝트가 제한한 범위를 일정 거리만큼 벗어나면 오브젝트 풀로 반환 
     private void LateUpdate()
     {
+        if (!gameObject.activeSelf) return; // 이미 비활성화된 경우 처리 중단
+        
         if (transform.position.y < stageData.LimitMin.y - destroyWeight ||
             transform.position.y > stageData.LimitMax.y + destroyWeight ||
             transform.position.x < stageData.LimitMin.x - destroyWeight ||
             transform.position.x > stageData.LimitMax.x + destroyWeight)
         {
-            Pool.Release(this.gameObject);
+            // 오브젝트 풀에 반환
+            if (Pool != null)
+            {
+                Pool.Release(this.gameObject);
+            }
         }
     }
 }

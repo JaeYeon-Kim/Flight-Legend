ef236670a351cf740fdba59a07196167dee33d4a
JaeYoun Kim <rlawodusx@naver.com>
2024-07-31 01:02:38 +0900

    Feat: 게임 종료 로직 추가

--- Assets/Scripts/UI/ButtonEvent.cs
+++ Assets/Scripts/UI/ButtonEvent.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-// 버튼 이벤트용 스크립트 
+// 메인 화면 - 버튼 이벤트용 스크립트 
 public class ButtonEvent : MonoBehaviour
 {
-    public void SceneLoader(string sceneName) {
+    public void SceneLoader(string sceneName)
+    {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
